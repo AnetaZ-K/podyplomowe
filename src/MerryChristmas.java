68a4054deb07bad335fbb92cdcbab3533de13637
Aneta Z-K <aneta.zka@icloud.com>
2020-12-11 22:42:05 +0100

    Initial commit

---
+++ src/MerryChristmas.java
@@ -0,0 +1,11 @@
+public class MerryChristmas {
+    public static void main(String[] args) {
+        System.out.println("________________________");
+        System.out.println("|*   *   *   *   *   * |");
+        System.out.println("|  *   *   *   *   *   |");
+        System.out.println("|*  Merry Christmas! * |");
+        System.out.println("|  *   *   *   *   *   |");
+        System.out.println("|*   *   *   *   *   * |");
+        System.out.println("------------------------");
+    }
+}
